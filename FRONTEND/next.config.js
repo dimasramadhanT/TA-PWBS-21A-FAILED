b93ab4bb1901c01131e7969863218f6a265720fb
dimasramadhanT <dimasmartsi8@gmail.com>
2024-01-23 09:04:51 -0800

    master : nambah publik untuk menabah fapikon

--- FRONTEND/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env : {
-        APIMahasiswa : 'http://127.0.0.1:8000/api/mahasiswa',
-        APIDosen : 'http://127.0.0.1:8000/api/dosen',
-    }
-}
-
-module.exports = nextConfig
